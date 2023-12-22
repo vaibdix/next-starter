8979e0c8b6a995205d821ac34479dc8c12172da1
vaibdix <dixitvaib@gmail.com>
2023-12-22 19:09:38 +0530

    next-14-starter

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section>
+      <div className='container'>
+        <h1 className='mt-4 text-3xl font-bold'>Next 14 starter kit</h1>
+      </div>
+    </section>
+  )
+}
